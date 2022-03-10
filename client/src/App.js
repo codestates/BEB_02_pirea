6dea6c234b434e8577f492db5b568521f2ae96f5
NonamedBread <lgh778923@gmail.com>
2022-03-10 13:23:16 +0900

    create-swap-UI

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Createswap from './component/Createswap';
+function App() {
+  return (
+    <div className="App">
+     <Createswap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Createswap.js
@@ -0,0 +1,53 @@
+import {useWeb3React} from "@web3-react/core";
+import { useState, useEffect, useCallback } from "react";
+import { injected } from "./Connector";
+import { isNoEthereumObject } from "./Errors";
+
+export default function Createswap(){
+    const { library, chainId, activate,  active, deactivate } = useWeb3React();
+
+    const handleConnect = () => {
+        if (active) {
+          deactivate();
+          return;
+        }
+        activate(injected, (error) => {
+          if (isNoEthereumObject(error))
+            window.open("https://metamask.io/download.html");
+        });
+      }
+   
+    
+   return (
+     <div>
+       <div>
+         <p>Createswap</p>
+         <button type="button" onClick={handleConnect}>
+           {active ? "disconnect" : "connect"}
+         </button>
+       </div>
+
+       <div>
+         <label for="fname">Token Address : </label>
+         <input type="text" id="Tokenaddress" name="fname" />
+       </div>
+
+       <div>
+         <label for="lname">Token ID : </label>
+         <input type="text" id="Tokenid" name="lname" />
+       </div>
+
+       <div>
+         <label for="lname">My Wallet Address : </label>
+         <input type="text" id="Walletaddress" name="lname" />
+       </div>
+       
+       <div>
+         <input type="submit" value="Submit" />
+       </div>
+     </div>
+   );
+
+   
+    
+}
