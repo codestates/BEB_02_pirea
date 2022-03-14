25583260c809d47cc848705a810a2a6a76117d10
NonamedBread <lgh778923@gmail.com>
2022-03-14 20:04:45 +0900

    dummy-server-confirm

--- server/index.js
+++ server/index.js
@@ -22,13 +22,9 @@ con.connect(function(err){
   //에러일 경우
   if (err) {throw err;}
   console.log(`Connected`);
-
-  //DB에 저장
-  // con
-
-  //DB에서 스키마 가져오기
+    //콘솔창에서 데이터 확인
   // const sql = "select * from userA"
-  // con.query(sql, funtion (err, result, fields) {
+  // con.query(sql, function (err, result, fields) {
 
   //   if (err) {throw err;}
   //   console.log(result)
@@ -36,5 +32,12 @@ con.connect(function(err){
 });
 
 
-app.get('/', (req, res) => res.send ('Hello World!'))
+app.get('/', (req, res) => {
+const sql = "select * from userA"
+con.query(sql, function (err, result, fields){
+if (err) {throw err;}
+res.send (result)})
+
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
