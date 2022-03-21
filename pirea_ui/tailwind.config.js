9ea00dc360f6db595612a6f57eae5853b2983a6f
mijin <>
2022-03-21 17:53:39 +0900

    add: test

--- pirea_ui/tailwind.config.js
+++ pirea_ui/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  //? 속성값 변경. 앞에 dark가 있으면 다크모드, 디폴트는 라이트모드
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
