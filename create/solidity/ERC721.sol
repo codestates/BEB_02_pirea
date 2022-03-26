1dbfc0a971ddb7dce12a52eb6cec59aea2e429d9
yoru.lee <yoru.lee7@gmail.com>
2022-03-27 02:23:56 +0900

    fix : mint solidity

--- create/solidity/ERC721.sol
+++ create/solidity/ERC721.sol
@@ -8,25 +8,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ERC721_Create is ERC721, Ownable {
       uint16 newItemId;
       uint16 _tokenIds;
-      // 초기값설정
+
+      mapping(address => uint256) private _recipient;
+      mapping(uint16 => uint16) public _newItemId;
+      // Question 1
+      // X,Y를 밖에서 받냐 안에서 지정하고시작하냐
+      mapping(uint16 => uint16) private _X;
+      mapping(uint16 => uint16) private _Y;
+      
     constructor() ERC721("Land", "Pixel") {
-      // 여기에 초기값설정가능? 함수실행시 한번만실행 변수의 값을 지정 해주는곳
+      uint256 pixelPrice = 10e18;
     }
     
-    // mint function
-    // there are few conditions
-    // 1. tokenId  0 to 199
-    function _mint(address recipient, string memory tokenURI) public onlyOwner returns (uint16) {
+    function _mint(address recipient, string memory tokenURI) public onlyOwner {
+        require(newItemId < 200);
         require(recipient != address(0), "ERC721: mint to the zero address");
         require(!_exists(newItemId), "ERC721: token already minted");
-        require(newItemId < 200);
-        // 좌표값을더해준다.tokenId값과 매핑
-        _tokenIds++;
 
-         newItemId = _tokenIds;
+        // xy를 증가시키면서 민팅을 실행한다.
+        for (uint16 X = 0; X < 11; X++) {
+          for (uint16 Y = 0; Y < 21; Y++) {
+          X = X - 4;
+          Y = Y - 9;
+          }
+        }
+          // Question 2
+          // ex11번째 토큰
+          // string corX = _tokenIds%10 -5
+          // string corY = _tokenIds%20 -10
+
         _mint(recipient, tokenURI);
+        
+        _tokenIds++;
+         newItemId = _tokenIds;
         
         return newItemId;
     }
-    //tokenId값이 들어오면 좌표를 리턴
 }
