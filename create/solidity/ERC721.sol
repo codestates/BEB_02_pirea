7aa84b6fd8dc646cc25ea189c4f30c85c1b9e984
yoru.lee <yoru.lee7@gmail.com>
2022-03-26 12:54:50 +0900

    fix : solidity code

--- create/solidity/ERC721.sol
+++ create/solidity/ERC721.sol
@@ -3,40 +3,30 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721_Create is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    constructor() ERC721("Land", "Pixel") {}
-
-    function tokenURI(
-        uint256 tokenId
-    ) 
-    public view
-      override(ERC721) returns (string memory) {
-        return super.tokenURI(tokenId);
+contract ERC721_Create is ERC721, Ownable {
+      uint16 newItemId;
+      uint16 _tokenIds;
+      // 초기값설정
+    constructor() ERC721("Land", "Pixel") {
+      // 여기에 초기값설정가능? 함수실행시 한번만실행 변수의 값을 지정 해주는곳
     }
     
     // mint function
     // there are few conditions
     // 1. tokenId  0 to 199
-    function _mint(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
-        
-        require(to != address(0), "ERC721: mint to the zero address");
-        require(!_exists(tokenId), "ERC721: token already minted");
-        require(newItemId < 200)
+    function _mint(address recipient, string memory tokenURI) public onlyOwner returns (uint16) {
+        require(recipient != address(0), "ERC721: mint to the zero address");
+        require(!_exists(newItemId), "ERC721: token already minted");
+        require(newItemId < 200);
+        // 좌표값을더해준다.tokenId값과 매핑
+        _tokenIds++;
 
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+         newItemId = _tokenIds;
+        _mint(recipient, tokenURI);
         
         return newItemId;
     }
+    //tokenId값이 들어오면 좌표를 리턴
 }
