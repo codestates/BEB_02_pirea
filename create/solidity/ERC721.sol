ae916d0815a9e382d68ad1ab4206b30fcaf0d0cb
yoru.lee <yoru.lee7@gmail.com>
2022-03-28 01:21:12 +0900

    fix : ERC721

--- create/solidity/ERC721.sol
+++ create/solidity/ERC721.sol
@@ -8,40 +8,54 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ERC721_Create is ERC721, Ownable {
       uint16 newItemId;
       uint16 _tokenIds;
+      int16 x;
+      int16 y;
 
       mapping(address => uint256) private _recipient;
       mapping(uint16 => uint16) public _newItemId;
-      // Question 1
-      // X,Y를 밖에서 받냐 안에서 지정하고시작하냐
-      mapping(uint16 => uint16) private _X;
-      mapping(uint16 => uint16) private _Y;
+      mapping(uint16 => int16) public _X;
+      mapping(uint16 => int16) public _Y;
       
     constructor() ERC721("Land", "Pixel") {
       uint256 pixelPrice = 10e18;
+       x = -4;
+       y = -9;
     }
     
-    function _mint(address recipient, string memory tokenURI) public onlyOwner {
-        require(newItemId < 200);
+    function _mint(address recipient, string memory tokenURI) public onlyOwner returns (uint16) {
+          // 총 개수는 200개
+          //x,y가 한계값을 넘어갔는지 확인한다. x가 5안넘게 , y가 10이안넘게
+        require(newItemId < 201);
         require(recipient != address(0), "ERC721: mint to the zero address");
         require(!_exists(newItemId), "ERC721: token already minted");
-
-        // xy를 증가시키면서 민팅을 실행한다.
-        for (uint16 X = 0; X < 11; X++) {
-          for (uint16 Y = 0; Y < 21; Y++) {
-          X = X - 4;
-          Y = Y - 9;
-          }
-        }
-          // Question 2
-          // ex11번째 토큰
-          // string corX = _tokenIds%10 -5
-          // string corY = _tokenIds%20 -10
+        require(x < 5 && y < 10);
 
         _mint(recipient, tokenURI);
         
-        _tokenIds++;
+        // xy를 증가시키면서 민팅을 실행한다.
+          // tokenIds로 x,y값을 구할 수 있음.
+          // uint16  coorX = _tokenIds%10 -5;
+          // uint16  coorY = _tokenIds%20 -10;
+          _tokenIds++;
+          x++;
+          y++;
+          // 한계값에서 초기화
+          if(x > 4) {
+            x = -4;
+          } 
+          if(y > 9) {
+            y = -9;
+          }
+
          newItemId = _tokenIds;
         
         return newItemId;
     }
+
+    //x,y값으로 토큰값 구하기
+    function returnTokenIds() public view returns ( int16 ) {
+      return (x + y + 13);
+    }
+    
+    
 }
